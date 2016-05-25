d18a0daab1a29bb2989badeda814ceec7d6e46b9
Adarnof <adarnof@gmail.com>
2016-05-24 22:12:36 -0400

    Handle list returned by dict.pop

--- eve_sso/managers.py
+++ eve_sso/managers.py
@@ -38,7 +38,7 @@ class CallbackRedirectManager(models.Manager):
         # remove scopes, if any
         get_dict.pop('scope', None)
         # build target url
-        url = get_dict.pop('redirect', '/')
+        url = get_dict.pop('redirect', ['/'])[0]
         url_parts = list(urlparse(url))
         url_parts[4] = get_dict.urlencode(safe='/')
         url = urlunparse(url_parts)
